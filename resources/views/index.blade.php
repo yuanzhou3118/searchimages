c3c812b6e538e1e1f74996d6b5f903b8c0f7e021
Sophia.Zhou <zhouyuan3118@163.com>
2016-05-04 18:25:40 +0800

    laravel 5.1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Search</title>
-
-
-
+    <link rel="icon" href="{{URL::asset('assets/image/Find.ico')}}">
+    <title>搜索首页</title>
+    <script src="{{URL::asset('assets/js/jquery-1.12.3.min.js')}}"></script>
     <style>
         html, body {
             height: 100%;
@@ -16,7 +15,9 @@
             width: 100%;
             display: table;
             font-weight: 100;
-            font-family: 'Lato';
+            font-family: 'Microsoft YaHei';
+            background: url("{{URL::asset('assets/image/background.jpg')}}") no-repeat;
+            background-size: cover;
         }
 
         .container {
@@ -30,21 +31,137 @@
             display: inline-block;
         }
 
+        /*.title {*/
+        /*font-size: 96px;*/
+        /*}*/
         .title {
+            color: #ffffff;
             font-size: 96px;
+            display: inline-block;
+            margin: 0;
         }
+
+        .find_icon {
+            display: inline-block;
+        }
+
+        .btn {
+            color: #fff;
+            background-color: #337ab7;
+            border-color: #2e6da4;
+            display: inline-block;
+            padding: 6px 12px;
+            margin-bottom: 0;
+            font-size: 14px;
+            font-weight: 400;
+            line-height: 1.42857143;
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: middle;
+            -ms-touch-action: manipulation;
+            touch-action: manipulation;
+            cursor: pointer;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            background-image: none;
+            border: 1px solid transparent;
+            border-radius: 4px;
+        }
+
+        input[type=file] {
+            position: absolute;
+            top: 0;
+            right: 0;
+            min-width: 100%;
+            min-height: 100%;
+            text-align: right;
+            filter: alpha(opacity=0);
+            opacity: 0;
+            background: none repeat scroll 0 0 transparent;
+            cursor: inherit;
+            display: block;
+        }
+
+        .btn-upload {
+            position: relative;
+            vertical-align: middle;
+            display: table-cell;
+            width: 88px;
+        }
+
+        .table {
+            display: table;
+            width: 100%;;
+        }
+
+        .form-control {
+            position: relative;
+            z-index: 2;
+            float: left;
+            margin-bottom: 0;
+            height: 34px;
+            padding: 6px 12px;
+            font-size: 14px;
+            line-height: 1.42857143;
+            color: #555;
+            background-color: #fff;
+            background-image: none;
+            border: 1px solid #ccc;
+            -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+            box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+            -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+            -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+            transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+            display: table-cell;
+            width: 79%;
+            border-radius: 0;
+        }
+
+        .btn_sub {
+            float: left;
+            position: relative;
+            vertical-align: middle;
+            display: table-cell;
+            width: 14%;
+            height: 48px;
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+
     </style>
 </head>
 <body>
 <div class="container">
     <div class="content">
-        <h1>图片搜索</h1>
-        <form method="post" action="{{URL::route('search')}}"  enctype="multipart/form-data">
+        <div>
+            <img src="{{URL::asset('assets/image/Find.png')}}" class="find_icon">
+            <p class="title">图片搜索</p>
+        </div>
+        <form method="post" action="{{URL::route('search')}}" enctype="multipart/form-data">
             {{csrf_field()}}
-            <input type="file" name="pic">
-            <button type="submit">搜索</button>
+            <div class="table">
+                <div class="btn btn-upload" style="border-top-right-radius:0;border-bottom-right-radius: 0;">
+                    <img src="{{URL::asset('assets/image/folder.png')}}" class="folder_icon">
+                    选择图片
+                    <input type="file" name="pic" id="f">
+                </div>
+                <div class="form-control"></div>
+                <button type="submit" class="btn btn_sub">搜索</button>
+            </div>
         </form>
     </div>
 </div>
+<script>
+
+    f.onchange = function (event) {
+        var folder_name = f.files[0].name;
+        $('.form-control').text(folder_name)
+
+    }
+
+
+</script>
 </body>
 </html>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>Search</title>
+    <link rel="icon" href="{{URL::asset('assets/image/Find.ico')}}">
+    <script src="{{URL::asset('assets/js/jquery-1.12.3.min.js')}}"></script>
+    <title>搜索结果页面</title>
 </head>
 <style>
     * {
@@ -74,13 +76,13 @@
     .wrapper{
         position: relative;
         padding-bottom: 20px;
-        margin: 57px 120px 0;
+        margin: 79px 120px 0;
         z-index: 1;
         min-width: 400px;
     }
     .header {
         position: fixed;
-        height: 56px;
+        height: 70px;
         width: 100%;
         border-bottom: 1px solid #ebebeb;
         top: 0;
@@ -124,15 +126,147 @@
     a{
         text-decoration: none;
     }
+    .btn{
+        color: #fff;
+        background-color: #337ab7;
+        border-color: #2e6da4;
+        display: inline-block;
+        padding: 6px 12px;
+        margin-bottom: 0;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 1.42857143;
+        text-align: center;
+        white-space: nowrap;
+        vertical-align: middle;
+        -ms-touch-action: manipulation;
+        touch-action: manipulation;
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        background-image: none;
+        border: 1px solid transparent;
+        border-radius: 4px;
+    }
+
+</style>
+<style>
+    .find_icon {
+        display: inline-block;
+    }
+
+    .btn {
+        color: #fff;
+        background-color: #337ab7;
+        border-color: #2e6da4;
+        display: inline-block;
+        padding: 6px 12px;
+        margin-bottom: 0;
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 1.42857143;
+        text-align: center;
+        white-space: nowrap;
+        vertical-align: middle;
+        -ms-touch-action: manipulation;
+        touch-action: manipulation;
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        background-image: none;
+        border: 1px solid transparent;
+        border-radius: 4px;
+    }
+
+    input[type=file] {
+        position: absolute;
+        top: 0;
+        right: 0;
+        min-width: 100%;
+        min-height: 100%;
+        text-align: right;
+        filter: alpha(opacity=0);
+        opacity: 0;
+        background: none repeat scroll 0 0 transparent;
+        cursor: inherit;
+        display: block;
+    }
+
+    .btn-upload {
+        position: relative;
+        vertical-align: middle;
+        display: table-cell;
+        width: 88px;
+    }
+
+    .table {
+        display: table;
+        width: 100%;;
+    }
+
+    .form-control {
+        position: relative;
+        z-index: 2;
+        float: left;
+        margin-bottom: 0;
+        height: 34px;
+        padding: 6px 12px;
+        font-size: 14px;
+        line-height: 1.42857143;
+        color: #555;
+        background-color: #fff;
+        background-image: none;
+        border: 1px solid #ccc;
+        -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+        box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+        -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+        -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+        transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+        display: table-cell;
+        width: 79%;
+        border-radius: 0;
+    }
+    .btn_sub{
+        float: left;
+        position: relative;
+        vertical-align: middle;
+        display: table-cell;
+        width: 14%;
+        height:48px;
+        border-top-left-radius:0;
+        border-bottom-left-radius: 0;
+    }
+    .form{
+        width: 60%;
+        display: inline-block;
+        margin: 10px;
+        overflow: hidden;
+    }
+    .index_link{
+        vertical-align: top;
+        display: inline-block;
+        margin-top: 8px;
+    }
 </style>
 <body>
 <section>
     <div class="header">
-        <a href="{{URL::route('index')}}"><h1 style="margin:10px;">图片搜索</h1></a>
-        <form method="post" action="{{URL::route('search')}}" enctype="multipart/form-data" class="search">
+        <a href="{{URL::route('index')}}" class="index_link"><h1 style="margin:10px;">图片搜索</h1></a>
+        <form method="post" action="{{URL::route('search')}}" enctype="multipart/form-data" class="form">
             {{csrf_field()}}
-            <input type="file" name="pic">
-            <button type="submit">搜索</button>
+            <div class="table">
+                <div class="btn btn-upload" style="border-top-right-radius:0;border-bottom-right-radius: 0;">
+                    <img src="{{URL::asset('assets/image/folder.png')}}" class="folder_icon">
+                    选择图片
+                    <input type="file" name="pic" id="f" >
+                </div>
+                <div class="form-control"></div>
+                <button type="submit" class="btn btn_sub">搜索</button>
+            </div>
         </form>
     </div>
     <div class="wrapper">
@@ -197,7 +331,13 @@
 </section>
 
 </body>
+<script>
+    f.onchange = function (event) {
+        var folder_name = f.files[0].name;
+        $('.form-control').text(folder_name)
 
+    }
+</script>
 
 </html>
 
